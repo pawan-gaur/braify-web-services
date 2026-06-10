e6d063ca03dadf0787263f0130095da9d77cc8b6
Pawan Gaur <pawangaur001@gmail.com>
2026-06-10 17:44:36 +0530

    Update backend limit for sending bulk email

--- src/main/java/com/braify/feature/bulkemail/service/BulkEmailService.java
+++ src/main/java/com/braify/feature/bulkemail/service/BulkEmailService.java
@@ -32,7 +32,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class BulkEmailService {
 
-    private static final int MAX_ROWS = 500;
+    private static final int MAX_ROWS = 5000;
 
     private final BulkEmailJobRepository   jobRepo;
     private final EmailTemplateRepository  emailTemplateRepo;
